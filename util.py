1f6a352a01b46463ae41f3c7dff58e55fea4e8ed
Yining Hong <evelinehong@hotmail.com>
2019-05-06 20:24:23 +0800

    Update util.py

--- util.py
+++ util.py
@@ -14,92 +14,9 @@ def custom_collate_fn(batch):
     bt_siz = len(batch)
     # sequence length only affects the memory requirement, otherwise longer is better
     pad_idx, max_seq_len = 52700, 60
-    #u_batch = []
-    #u_lens = []
-    #t = []
-    #u1_biatch, u2_batch, u3_batch = [], [], []
-    #u1_lens, u2_lens, u3_lens = np.zeros(bt_siz, dtype=int), np.zeros(bt_siz, dtype=int), np.zeros(bt_siz, dtype=int)
+    
+   
 
-    # these store the max sequence lengths for the batch
-    #l_u1, l_u2, l_u3 = 0, 0, 0
-        
-    #l_u = []
-    #find the maximum turns
-    #for i, (d, cl_u) in enumerate(batch):
-    #    for cl_utter in cl_u:
-    #        cl_utter = min(cl_utter, max_seq_len)
-    #        l_utter = 0
-    #        if cl_utter > l_utter:
-    #            l_utter = cl_utter
-    #        utter_lens = np.zeros(bt_siz, dtype = int)
-    #        utter_lens
-    #        l_u.append(l_utter)
-    #    for j, (utter) in enumerate(d.u):
-    #        utter_batch = []
-    #        utter_batch.append(torch.LongTensor(utter))
-    #        t.append(utter_batch)
-    #        utter_batch = Variable(torch.ones(bt_siz, l_u[j]).long * pad_idx)
-    #        u_batch.append(utter_batch)
-    #       end_tok = torch.LongTensor([2])
-
-    #for i in range(bt_siz):
-    #    for j,turn in t:
-    #        seq, cur_l = turn[i], turn[i].size()
-    #        if cur_l <= l_u[j]:
-    #            u_batch[i, :cul_l][j].data.copy_(seq[:cur_l]) 
-    #        else: 
-    #            u_batch[i, :][j].data.copy_(torch.cat((seq[:l_u[j]-1], end_tok),0))
-
-    # for i, (d, cl_u1, cl_u2, cl_u3) in enumerate(batch):
-    #    cl_u1 = min(cl_u1, max_seq_len)
-    #    cl_u2 = min(cl_u2, max_seq_len)
-    #    cl_u3 = min(cl_u3, max_seq_len)
-
-    #    if cl_u1 > l_u1:
-    #        l_u1 = cl_u1
-    #    u1_batch.append(torch.LongTensor(d.u1))
-    #    u1_lens[i] = cl_u1
-
-    #    if cl_u2 > l_u2:
-    #        l_u2 = cl_u2
-    #    u2_batch.append(torch.LongTensor(d.u2))
-    #    u2_lens[i] = cl_u2
-
-    #    if cl_u3 > l_u3:
-    #        l_u3 = cl_u3
-    #    u3_batch.append(torch.LongTensor(d.u3))
-    #    u3_lens[i] = cl_u3
-
-    # t1, t2, t3 = u1_batch, u2_batch, u3_batch
-
-    #u1_batch = Variable(torch.ones(bt_siz, l_u1).long() * pad_idx)
-    #u2_batch = Variable(torch.ones(bt_siz, l_u2).long() * pad_idx)
-    #u3_batch = Variable(torch.ones(bt_siz, l_u3).long() * pad_idx)
-    #end_tok = torch.LongTensor([2])
-
-    #for i in range(bt_siz):
-    #    seq1, cur1_l = t1[i], t1[i].size(0)
-    #    if cur1_l <= l_u1:
-    #        u1_batch[i, :cur1_l].data.copy_(seq1[:cur1_l])
-    #    else:
-    #        u1_batch[i, :].data.copy_(torch.cat((seq1[:l_u1-1], end_tok), 0))
-
-    #    seq2, cur2_l = t2[i], t2[i].size(0)
-    #    if cur2_l <= l_u2:
-    #        u2_batch[i, :cur2_l].data.copy_(seq2[:cur2_l])
-    #    else:
-    #        u2_batch[i, :].data.copy_(torch.cat((seq2[:l_u2-1], end_tok), 0))
-
-    #    seq3, cur3_l = t3[i], t3[i].size(0)
-    #    if cur3_l <= l_u3:
-    #        u3_batch[i, :cur3_l].data.copy_(seq3[:cur3_l])
-    #    else:
-    #        u3_batch[i, :].data.copy_(torch.cat((seq3[:l_u3-1], end_tok), 0))
-
-    #sort1, sort2, sort3 = np.argsort(u1_lens*-1), np.argsort(u2_lens*-1), np.argsort(u3_lens*-1)
-    # cant call use_cuda here because this function block is used in threading calls
-
-    #return u1_batch[sort1, :], u1_lens[sort1], u2_batch[sort2, :], u2_lens[sort2], u3_batch[sort3, :], u3_lens[sort3]
     maxturnsnumber = 0
     minturn = 100
     turnsnumbers = []
